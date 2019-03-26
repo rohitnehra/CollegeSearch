6b6f49af875898a061199d05b7618c11da5a489c
Gabolicious <ohgabriel5@gmail.com>
2019-03-26 11:13:16 -0400

    Added PHP

---
+++ searchresults.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+        <meta charset="utf-8" />
+        <title>College Search</title>
+        <link rel="stylesheet" type="text/css" href="main.css" />
+    </head>
+<body>
+    You searched for <?php echo $_GET["instnm"]; ?><br>
+    In city <?php echo $_GET["city"]; ?>
+<body>
+</html>
